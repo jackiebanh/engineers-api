6c0c12e39058d8b571cccdc40a822409c9eb11d2
Jackie Banh <jabanh@deloitte.com.au>
2021-10-06 16:50:03 +1100

    add simple endpoint to subscriber

--- app/subscriber.js
+++ app/subscriber.js
@@ -1,4 +1,8 @@
 import redis from 'redis';
+import express from 'express';
+
+const app = express().use(express.json())
+const port = process.env.PORT || 3000
 
 var subscriber = redis.createClient(`${process.env.REDIS_URL}`);
 
@@ -7,3 +11,11 @@ subscriber.on("message", function (channel, message) {
 });
 
 subscriber.subscribe("notification");
+
+app.get('/ping', async (req, res) => {
+  res.send('pong!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
